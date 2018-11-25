ddd4acf039a4d80988ef01c3ab764f53a8350681
KevinPsy <43185784+KevinPsy@users.noreply.github.com>
2018-11-25 11:45:10 -0500

    Add files via upload

--- lab11.R
+++ lab11.R
@@ -2,49 +2,52 @@ library(tidyverse)
 
 #Q1
 rambinom<-function(n,p){
-  x<-NULL
-  x<-runif(n,min=0,max=1)
-  return(sum(x<p))
+  x <- NULL
+  x <- runif(n, min = 0, max = 1)
+  return(sum(x < p))
   
 }
 
 
-rambinom(100,0.7)  
-rbinom(1,100,0.7)
+rambinom(100, 0.7)  
+rbinom(1, 100, 0.7)
 
 #Q2
 library(microbenchmark)
 
-microbenchmark(rambinom(100,0.7),rbinom(1,100,0.7))
+microbenchmark(rambinom(100, 0.7), rbinom(1, 100, 0.7))
 
 #Q3
 
-x1<-runif(50,min = 20,max=40)
+x1 <- runif(50, min = 20, max = 40)
 
-y1<-(15+0.4*x1+rnorm(50,0,3))
+y1 <- (15 + 0.4*x1 + rnorm(50, 0,3))
 
-lmodel<-lm(y1~x1)
+lmodel <- lm(y1~x1)
+
 plot(x1,y1)
 abline(lm(y1~x1))
+
 summary(lm(y1~x1))
+
 ggplot(lmodel) + 
   geom_point(aes(x=.fitted, y=.resid))
 
 #Q4
 ramnorm<-function(n){
-  u1<-runif(n,0,1)
-  u2<-runif(n,0,1)
+  u1 <- runif(n, 0, 1)
+  u2 <- runif(n, 0, 1)
    
-  r<-(-2*log(u1))^0.5
-  theta<-2*pi*u2
+  r <- (-2*log(u1))^0.5
+  theta <- 2*pi*u2
   
-  rv<-c(r*sin(theta),r*cos(theta))
+  rv <- c(r*sin(theta),r*cos(theta))
   return(rv[1:(length(rv)/2)])
 }
 
 muller<-ramnorm(100)
-normal<-rnorm(100,0,1)
+normal<-rnorm(100, 0, 1)
 
 ggplot()+
-  geom_density(aes(normal,fill="green",alpha=0.5))+
-  geom_density(aes(muller,fill="red",alpha=0.5))
+  geom_density(aes(normal, fill="normal", alpha=0.5))+
+  geom_density(aes(muller, fill="boxmuller", alpha=0.5))
